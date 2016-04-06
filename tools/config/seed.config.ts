37bb0eba28e5a798232f3090599dee30c8a2bad2
marcgr <marc.rempel@oregonstate.edu>
2016-04-06 13:41:53 -0700

    Modified project.config.ts to support new env deployment settings.

--- tools/config/seed.config.ts
+++ tools/config/seed.config.ts
@@ -41,134 +41,128 @@ export class SeedConfig {
     VERSION = appVersion();
 
     CSS_PROD_BUNDLE = 'all.css';
-  JS_PROD_SHIMS_BUNDLE = 'shims.js';
+    JS_PROD_SHIMS_BUNDLE = 'shims.js';
     JS_PROD_APP_BUNDLE = 'app.js';
 
     VERSION_NPM = '2.14.2';
     VERSION_NODE = '4.0.0';
 
-  CODELYZER_RULES      = customRules();
+    CODELYZER_RULES = customRules();
 
-  NPM_DEPENDENCIES: InjectableDependency[] = [
-    { src: 'systemjs/dist/system-polyfills.src.js', inject: 'shims', env: ENVIRONMENTS.DEVELOPMENT },
-    { src: 'reflect-metadata/Reflect.js', inject: 'shims', env: ENVIRONMENTS.DEVELOPMENT },
-    { src: 'es6-shim/es6-shim.js', inject: 'shims', env: ENVIRONMENTS.DEVELOPMENT },
-    { src: 'systemjs/dist/system.src.js', inject: 'shims', env: ENVIRONMENTS.DEVELOPMENT },
-    { src: 'angular2/bundles/angular2-polyfills.js', inject: 'shims' },
+
+
+    NPM_DEPENDENCIES: InjectableDependency[] = [
+        { src: 'systemjs/dist/system-polyfills.src.js', inject: 'shims', env: ENVIRONMENTS.DEVELOPMENT },
+        { src: 'reflect-metadata/Reflect.js', inject: 'shims', env: ENVIRONMENTS.DEVELOPMENT },
+        { src: 'zone.js/dist/zone.js', inject: 'shims', env: ENVIRONMENTS.DEVELOPMENT },
+        { src: 'es6-shim/es6-shim.js', inject: 'shims', env: ENVIRONMENTS.DEVELOPMENT },
+        { src: 'systemjs/dist/system.src.js', inject: 'shims', env: ENVIRONMENTS.DEVELOPMENT },
+        //{ src: 'angular2/bundles/angular2-polyfills.js', inject: 'shims' },
         { src: 'rxjs/bundles/Rx.js', inject: 'libs' },
         { src: 'angular2/bundles/angular2.js', inject: 'libs' },
         { src: 'angular2/bundles/router.js', inject: 'libs' },
         { src: 'angular2/bundles/http.js', inject: 'libs' },
         { src: 'bootstrap/dist/css/bootstrap.css', inject: true }
-    ]);
-
-    PROD_NPM_DEPENDENCIES: InjectableDependency[] = normalizeDependencies([
-        { src: 'systemjs/dist/system-polyfills.src.js', inject: 'shims' },
-        { src: 'reflect-metadata/Reflect.js', inject: 'shims' },
-        { src: 'es6-shim/es6-shim.min.js', inject: 'shims' },
-        { src: 'systemjs/dist/system.js', inject: 'shims' },
-        { src: 'angular2/bundles/angular2-polyfills.min.js', inject: 'libs' },
-
-    ]);
-
-  // Declare local files that needs to be injected
-  APP_ASSETS: InjectableDependency[] = [
-    { src: `${this.ASSETS_SRC}/main.css`, inject: true, vendor: false }
-  ];
+    ];
+    //// Declare local files that needs to be injected
+    APP_ASSETS: InjectableDependency[] = [];
+    //project specfic dependencies in project.config.cs
+    //DEV_APP_ASSETS: InjectableDependency[] = [];
+    //PROD_APP_ASSETS: InjectableDependency[] = [];
 
 
-  get PROD_DEPENDENCIES(): InjectableDependency[] {
-    console.warn('The property "PROD_DEPENDENCIES" is deprecated. Consider using "DEPENDENCIES" instead.');
-    return normalizeDependencies(this.NPM_DEPENDENCIES.filter(filterDependency.bind(null, ENVIRONMENTS.PRODUCTION)))
-      .concat(this.APP_ASSETS.filter(filterDependency.bind(null, ENVIRONMENTS.PRODUCTION)));
-  }
-
-  get DEV_DEPENDENCIES(): InjectableDependency[] {
-    console.warn('The property "DEV_DEPENDENCIES" is deprecated. Consider using "DEPENDENCIES" instead.');
-    return normalizeDependencies(this.NPM_DEPENDENCIES.filter(filterDependency.bind(null, ENVIRONMENTS.DEVELOPMENT)))
-      .concat(this.APP_ASSETS.filter(filterDependency.bind(null, ENVIRONMENTS.DEVELOPMENT)));
-  }
-
-  set DEV_DEPENDENCIES(val: InjectableDependency[]) {
-    console.warn('The property "DEV_DEPENDENCIES" is deprecated. Consider using "DEPENDENCIES" instead.');
-  }
-
-  set PROD_DEPENDENCIES(val: InjectableDependency[]) {
-    console.warn('The property "PROD_DEPENDENCIES" is deprecated. Consider using "DEPENDENCIES" instead.');
-  }
-
-  get DEV_NPM_DEPENDENCIES(): InjectableDependency[] {
-    console.warn('The property "DEV_NPM_DEPENDENCIES" is deprecated. Consider using "DEPENDENCIES" instead.');
-    return normalizeDependencies(this.NPM_DEPENDENCIES.filter(filterDependency.bind(null, ENVIRONMENTS.DEVELOPMENT)));
-  }
-  get PROD_NPM_DEPENDENCIES(): InjectableDependency[] {
-    console.warn('The property "PROD_NPM_DEPENDENCIES" is deprecated. Consider using "DEPENDENCIES" instead.');
-    return normalizeDependencies(this.NPM_DEPENDENCIES.filter(filterDependency.bind(null, ENVIRONMENTS.PRODUCTION)));
-  }
-  set DEV_NPM_DEPENDENCIES(value: InjectableDependency[]) {
-    console.warn('The property "DEV_NPM_DEPENDENCIES" is deprecated. Consider using "DEPENDENCIES" instead.');
-    const notDev = this.NPM_DEPENDENCIES.filter(d => !filterDependency(ENVIRONMENTS.DEVELOPMENT, d));
-    this.NPM_DEPENDENCIES = notDev.concat(value);
-  }
-  set PROD_NPM_DEPENDENCIES(value: InjectableDependency[]) {
-    console.warn('The property "PROD_NPM_DEPENDENCIES" is deprecated. Consider using "DEPENDENCIES" instead.');
-    const notProd = this.NPM_DEPENDENCIES.filter(d => !filterDependency(ENVIRONMENTS.PRODUCTION, d));
-    this.NPM_DEPENDENCIES = notProd.concat(value);
-  }
-
-  get DEPENDENCIES(): InjectableDependency[] {
-    return normalizeDependencies(this.NPM_DEPENDENCIES.filter(filterDependency.bind(null, this.ENV)))
-      .concat(this.APP_ASSETS.filter(filterDependency.bind(null, this.ENV)));
-  }
-
-  // ----------------
-  // SystemsJS Configuration.
-  protected SYSTEM_CONFIG_DEV = {
-    defaultJSExtensions: true,
-    packageConfigPaths: [`${this.APP_BASE}node_modules/*/package.json`],
-    paths: {
-      [this.BOOTSTRAP_MODULE]: `${this.APP_BASE}${this.BOOTSTRAP_MODULE}`,
-      'angular2/*': `${this.APP_BASE}angular2/*`,
-      'rxjs/*': `${this.APP_BASE}rxjs/*`,
-      '*': `${this.APP_BASE}node_modules/*`
-    },
-    packages: {
-      angular2: { defaultExtension: false },
-      rxjs: { defaultExtension: false }
+    get PROD_DEPENDENCIES(): InjectableDependency[] {
+        console.warn('The property "PROD_DEPENDENCIES" is deprecated. Consider using "DEPENDENCIES" instead.');
+        return normalizeDependencies(this.NPM_DEPENDENCIES.filter(filterDependency.bind(null, ENVIRONMENTS.PRODUCTION)))
+            .concat(this.APP_ASSETS.filter(filterDependency.bind(null, ENVIRONMENTS.PRODUCTION)));
     }
-  };
 
-  SYSTEM_CONFIG = this.SYSTEM_CONFIG_DEV;
-
-  SYSTEM_BUILDER_CONFIG = {
-    defaultJSExtensions: true,
-    paths: {
-      [`${this.TMP_DIR}/*`]: `${this.TMP_DIR}/*`,
-      '*': 'node_modules/*'
+    get DEV_DEPENDENCIES(): InjectableDependency[] {
+        console.warn('The property "DEV_DEPENDENCIES" is deprecated. Consider using "DEPENDENCIES" instead.');
+        return normalizeDependencies(this.NPM_DEPENDENCIES.filter(filterDependency.bind(null, ENVIRONMENTS.DEVELOPMENT)))
+            .concat(this.APP_ASSETS.filter(filterDependency.bind(null, ENVIRONMENTS.DEVELOPMENT)));
     }
-  };
 
-  // ----------------
-  // Autoprefixer configuration.
-  BROWSER_LIST = [
-    'ie >= 10',
-    'ie_mob >= 10',
-    'ff >= 30',
-    'chrome >= 34',
-    'safari >= 7',
-    'opera >= 23',
-    'ios >= 7',
-    'android >= 4.4',
-    'bb >= 10'
-  ];
-  getEnvDependencies() {
-    console.warn('The "getEnvDependencies" method is deprecated. Consider using "DEPENDENCIES" instead.');
-    if (this.ENV === 'prod') {
-      return this.PROD_DEPENDENCIES;
-    } else {
-      return this.DEV_DEPENDENCIES;
+    set DEV_DEPENDENCIES(val: InjectableDependency[]) {
+        console.warn('The property "DEV_DEPENDENCIES" is deprecated. Consider using "DEPENDENCIES" instead.');
     }
-  }
+
+    set PROD_DEPENDENCIES(val: InjectableDependency[]) {
+        console.warn('The property "PROD_DEPENDENCIES" is deprecated. Consider using "DEPENDENCIES" instead.');
+    }
+
+    get DEV_NPM_DEPENDENCIES(): InjectableDependency[] {
+        console.warn('The property "DEV_NPM_DEPENDENCIES" is deprecated. Consider using "DEPENDENCIES" instead.');
+        return normalizeDependencies(this.NPM_DEPENDENCIES.filter(filterDependency.bind(null, ENVIRONMENTS.DEVELOPMENT)));
+    }
+    get PROD_NPM_DEPENDENCIES(): InjectableDependency[] {
+        console.warn('The property "PROD_NPM_DEPENDENCIES" is deprecated. Consider using "DEPENDENCIES" instead.');
+        return normalizeDependencies(this.NPM_DEPENDENCIES.filter(filterDependency.bind(null, ENVIRONMENTS.PRODUCTION)));
+    }
+    set DEV_NPM_DEPENDENCIES(value: InjectableDependency[]) {
+        console.warn('The property "DEV_NPM_DEPENDENCIES" is deprecated. Consider using "DEPENDENCIES" instead.');
+        const notDev = this.NPM_DEPENDENCIES.filter(d => !filterDependency(ENVIRONMENTS.DEVELOPMENT, d));
+        this.NPM_DEPENDENCIES = notDev.concat(value);
+    }
+    set PROD_NPM_DEPENDENCIES(value: InjectableDependency[]) {
+        console.warn('The property "PROD_NPM_DEPENDENCIES" is deprecated. Consider using "DEPENDENCIES" instead.');
+        const notProd = this.NPM_DEPENDENCIES.filter(d => !filterDependency(ENVIRONMENTS.PRODUCTION, d));
+        this.NPM_DEPENDENCIES = notProd.concat(value);
+    }
+
+    get DEPENDENCIES(): InjectableDependency[] {
+        return normalizeDependencies(this.NPM_DEPENDENCIES.filter(filterDependency.bind(null, this.ENV)))
+            .concat(this.APP_ASSETS.filter(filterDependency.bind(null, this.ENV)));
+    }
+
+    // ----------------
+    // SystemsJS Configuration.
+    protected SYSTEM_CONFIG_DEV = {
+        defaultJSExtensions: true,
+        packageConfigPaths: [`${this.APP_BASE}node_modules/*/package.json`],
+        paths: {
+            [this.BOOTSTRAP_MODULE]: `${this.APP_BASE}${this.BOOTSTRAP_MODULE}`,
+            'angular2/*': `${this.APP_BASE}angular2/*`,
+            'rxjs/*': `${this.APP_BASE}rxjs/*`,
+            '*': `${this.APP_BASE}node_modules/*`
+        },
+        packages: {
+            angular2: { defaultExtension: false },
+            rxjs: { defaultExtension: false }
+        }
+    };
+
+    SYSTEM_CONFIG = this.SYSTEM_CONFIG_DEV;
+
+    SYSTEM_BUILDER_CONFIG = {
+        defaultJSExtensions: true,
+        paths: {
+            [`${this.TMP_DIR}/*`]: `${this.TMP_DIR}/*`,
+            '*': 'node_modules/*'
+        }
+    };
+
+    // ----------------
+    // Autoprefixer configuration.
+    BROWSER_LIST = [
+        'ie >= 10',
+        'ie_mob >= 10',
+        'ff >= 30',
+        'chrome >= 34',
+        'safari >= 7',
+        'opera >= 23',
+        'ios >= 7',
+        'android >= 4.4',
+        'bb >= 10'
+    ];
+    getEnvDependencies() {
+        console.warn('The "getEnvDependencies" method is deprecated. Consider using "DEPENDENCIES" instead.');
+        if (this.ENV === 'prod') {
+            return this.PROD_DEPENDENCIES;
+        } else {
+            return this.DEV_DEPENDENCIES;
+        }
+    }
 }
 
 
